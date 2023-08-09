c42c1baccfe0502d45f8d8ded6712cfbed89bfcd
Earrrrrth <141677113+Phornpawit11@users.noreply.github.com>
2023-08-10 01:39:38 +0700

    Upadte_UserContext

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Mainavigation from './Navigate/index';
+import { useCachedResources } from './Hook';
+import UserProvider from './Hook/UserProvider'
 export default function App() {
+  if (useCachedResources() == false) {
+    return null;
+  }
   return (
+    <UserProvider>
       <Mainavigation></Mainavigation>
+    </UserProvider>
   );
 }
 
